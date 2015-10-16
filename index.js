5cca9efbce09d5afe9bd58c00c4969a7b4d3f9d5
Nick Breaton <nbreaton@users.noreply.github.com>
2015-10-16 09:38:32 -0400

    Fix tabbing.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/nickbreaton', function (req, res) {
