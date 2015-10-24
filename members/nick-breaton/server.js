86342112670e47b461a4e94ea4eb45264fbe9cda
Nick Breaton <nick@breaton.com>
2015-10-23 22:53:18 -0400

    Include members pages.

---
+++ members/nick-breaton/server.js
@@ -0,0 +1,3 @@
+module.exports = function (req, res, next) {
+  res.end("Hello from nick!");
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // import node modules
 var express = require("express");
+var fs = require("fs");
 var path = require("path");
 
 // configure express instance
@@ -10,7 +11,13 @@ server.set("view-engine", "ejs");
 server.set("views", path.join(__dirname, "views/"));
 
 // include all members pages
-
+fs.readdir(path.join(__dirname, "..", "members"), function (err, files) {
+  files.forEach(function (file) {
+    var memberModulePath = path.join(__dirname, "..", "members", file, "server.js");
+    var memberMiddleware = require(memberModulePath);
+    server.use("/member/" + file, memberMiddleware);
+  });
+});
 
 server.listen(server.get("port"), function () {
   console.log("Server is running at http://localhost:" + server.get("port") + "/");
