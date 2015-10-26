562218222313b926d8feebcd30e7aae6a7480683
Nick Breaton <nick@breaton.com>
2015-10-26 10:00:30 -0400

    Change router to make more clear.

--- server/routes/members.js
+++ server/routes/members.js
@@ -3,15 +3,15 @@ var express = require("express");
 var path = require("path");
 
 var membersDir = path.join(__dirname, "../../members");
-
-// export a router
-module.exports = express.Router();
+var router = express.Router();
 
 fs.readdir(membersDir, function (err, members) {
   if (!err) {
     members.forEach(function (member) {
       var memberPath = path.join(membersDir, member, "server/index");
-      module.exports.use("/" + member + "/", require(memberPath));
+      router.use("/" + member + "/", require(memberPath));
     });
   }
 });
+
+module.exports = router;
