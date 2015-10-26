26802a733a569a29eec038f2dedb211640bacfe0
Nick Breaton <nick@breaton.com>
2015-10-26 12:19:48 -0400

    Add features to SCSS build process.

--- commands/build.js
+++ commands/build.js
@@ -1,17 +1,26 @@
 var autoprefixer = require("gulp-autoprefixer");
 var clean = require("gulp-clean");
 var gulp = require("./gulp");
+var minify = require("gulp-minify-css");
 var path = require("path");
 var plumber = require("gulp-plumber");
 var rename = require("gulp-rename");
 var scss = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var strip = require('gulp-strip-comments');
 
 var outDir = path.join(__dirname, "../build");
+var production = false;
 
 gulp.task("build", [
   "build:clean",
   "build:styles"
 ]);
+
+gulp.task("build:production", function () {
+  production = true;
+  gulp.start("build");
+});
 
 gulp.task("build:clean", function () {
   return gulp.src(path.join(outDir, "*"))
@@ -19,13 +28,25 @@ gulp.task("build:clean", function () {
 });
 
 gulp.task("build:styles", function () {
-  gulp.src(path.join(__dirname, "../styles/index.scss"))
+  var stream = gulp.src(path.join(__dirname, "../styles/index.scss"))
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(scss())
+    .pipe(autoprefixer({
+      browsers: ["> 2%"]
+    }))
+    .pipe(minify())
+    .pipe(sourcemaps.write())
     .pipe(rename("index.css"))
     .pipe(gulp.dest(outDir));
+
+  if (production) {
+    stream = stream.pipe(strip());
+  }
+
+  stream.pipe(gulp.dest(outDir));
 });
 
 module.exports = function () {
-  gulp.start("build");
+  gulp.start("build:production");
 }
