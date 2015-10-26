28bd0442e93dd2047ec58888749ded452d95e9f4
Nick Breaton <nick@breaton.com>
2015-10-26 11:36:52 -0400

    Create easier to map routes to modules.

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ var path = require("path");
 
 var server = express();
 
-var routes = {
-  members: require("./routes/members")
-}
-
-server.use("/member/", routes.members);
+route("/member/", "./routes/members");
 
 server.listen(3000, function () {
   console.log("Server is running at http://localhost:3000/");
 });
+
+// map route to module
+function route (route, modulePath) {
+  server.use(route, require(modulePath));
+}
