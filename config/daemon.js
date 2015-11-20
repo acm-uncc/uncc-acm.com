6cdca990cad00ff656005c8f69a88849bd19ee6f
Nick Breaton <nick@breaton.com>
2015-11-20 15:42:07 -0500

    Fix exit error with nodemon.

--- config/daemon.js
+++ config/daemon.js
@@ -1,7 +1,11 @@
 var nodemon = require('nodemon');
 var path = require('path');
 
-nodemon({
+var daemon = nodemon({
   script: path.join(__dirname, './server.js'),
   watch: path.join(__dirname, '../api')
+});
+
+daemon.once('exit', function() {
+  process.exit();
 });
