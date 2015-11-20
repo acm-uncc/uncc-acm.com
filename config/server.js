f100b00492b81bc72274f5c88971c5f88f1fb4c0
Nick Breaton <nick@breaton.com>
2015-11-20 02:05:15 -0500

    Modulate API routes.

--- config/server.js
+++ config/server.js
@@ -10,6 +10,10 @@ app.set('port', 3000);
 if (process.env.NODE_ENV == 'development') dev(app);
 if (process.env.NODE_ENV == 'production') prod(app);
 
+// require api
+var api = require('./api');
+api(app);
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
