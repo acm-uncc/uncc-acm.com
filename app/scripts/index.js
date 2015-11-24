2b72e8794b5d37c512ff902e90ce761d2ceaf085
Nick Breaton <nick@breaton.com>
2015-11-23 21:39:43 -0500

    Link unimplemted pages to template.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,46 +7,26 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
   // remove the hash (#) from the url
   $locationProvider.html5Mode(true);
 
-  /*
-    New pages must be registered here.
-
-    A url and template to display at that url must be provided.
-
-    An angular controller can also be provided in order to interact with the
-    template using JavaScript.
-
-    This is from the ui-router:
-    http://bit.ly/1N01wM5
-  */
+  // custom pages
   $stateProvider
     .state('home', {
       url: '/',
       templateUrl: 'pages/home.html'
     })
-    .state('calendar', {
-      url: '/calendar',
-      templateUrl: 'pages/calendar.html'
-    })
-    .state('projects', {
-      url: '/projects',
-      template: 'projects'
-    })
-    .state('awards', {
-      url: '/awards',
-      template: 'awards'
-    })
-    .state('members', {
-      url: '/members',
-      template: 'members'
-    })
-    .state('contact', {
-      url: '/contact',
-      template: 'contact'
-    })
     .state('404', {
       url: '/404',
       templateUrl: 'pages/error.html'
     })
+
+  var pages = [ 'calendar', 'projects', 'awards', 'members', 'contact' ];
+
+  pages.forEach(function (page) {
+    $stateProvider
+      .state(page, {
+        url: '/' + page,
+        templateUrl: 'pages/' + page + '.html'
+      });
+    });
 
   // maintain URL when defaulting to not found page
   $urlRouterProvider.otherwise(function ($injector) {
